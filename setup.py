4fb9ffec15afa03fbd9396a6d5b07c83bcb4f58f
G-Beast <tanmaysaxena58@gmail.com>
2024-10-28 18:19:58 -0400

    made changes to pipfile and setup

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 setup(
     name='project1',
@@ -9,18 +8,5 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    install_requires=[
-        "spacy",
-        "torch",
-        "transformers",
-        "pytest",
-        "en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.5.0/en_core_web_sm-3.5.0.tar.gz"
-    ]
 )
 
-# Download model within setup if needed
-try:
-    import spacy
-    spacy.cli.download("en_core_web_sm")
-except ImportError:
-    pass
